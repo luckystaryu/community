fa4327a3c4f31299baf6e0445f3a839da3be8434
yuxuqi <465078693@qq.com>
2019-07-17 09:15:44 +0800

    alter index

--- src/main/java/com/zjpl/community/Controllers/IndexController.java
+++ src/main/java/com/zjpl/community/Controllers/IndexController.java
@@ -2,18 +2,15 @@ package com.zjpl.community.Controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name",required =false, defaultValue = "Word") String name, Model model){
-       model.addAttribute("name",name);
+public class IndexController {
+    @GetMapping("/")
+    public String Index(){
        return "index";
     }
 }
